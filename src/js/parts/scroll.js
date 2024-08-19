39fc5378062d5fae7ee15edc2f269675e6df190b
Uzurch1k <galcenkovlad7@gmail.com>
2024-08-19 22:06:34 +0300

    finish footer

---
+++ src/js/parts/scroll.js
@@ -0,0 +1,8 @@
+const scrollToTopBtn = document.querySelector('.footer__scroll-btn');
+
+scrollToTopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
