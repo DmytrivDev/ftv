ebcf8c10d16bbd1c92367327c8238f8660aab811
Uzurch1k <galcenkovlad7@gmail.com>
2024-08-22 16:58:39 +0300

    fix

--- src/js/parts/accordion.js
+++ src/js/parts/accordion.js
@@ -9,16 +9,3 @@ if (accordionContainer) {
     openOnInit: [0],
   });
 }
-
-// const accordionItems = document.querySelectorAll('.services__item');
-
-// accordionItems.forEach(item => {
-//   item.addEventListener('click', function () {
-//     const imgSrc = this.dataset.src;
-//     const imgElement = document.querySelector('.services__img-wrapp img');
-
-//     if (imgSrc && imgElement) {
-//       imgElement.src = imgSrc;
-//     }
-//   });
-// });
--- src/js/parts/splide.js
+++ src/js/parts/splide.js
@@ -1,52 +1,40 @@
 import Splide from '@splidejs/splide';
 import '@splidejs/splide/css';
 
-const instSecondSlider = () => {
-  const slider = document.querySelector('.process-splide');
+let splideInstance;
 
-  if (slider) {
+const instSecondSlider = () => {
+  const slider = document.querySelector('.process .splide');
+
+  if (slider && !splideInstance) {
     const options = {
       type: 'slide',
       speed: 1000,
-      pagination: false,
       updateOnMove: true,
-      perPage: 2,
+      pagination: true,
+      arrows: false,
+      perPage: 1,
       perMove: 1,
-      gap: '1.88rem',
-      breakpoints: {
-        960: {
-          perPage: 2,
-          perMove: 1,
-          gap: '1rem',
-        },
-        760: {
-          arrows: false,
-          pagination: true,
-          perPage: 1,
-        },
-      },
+      gap: '1.375rem',
     };
 
-    new Splide(slider, options).mount();
+    splideInstance = new Splide(slider, options).mount();
   }
-
-  // arrowsClicker();
 };
-instSecondSlider();
 
-// function arrowsClicker() {
-//   const arrows = document.querySelectorAll('.reviews-arrow');
-//   const container = document.querySelector('.reviews__box');
+const destroySlider = () => {
+  if (splideInstance) {
+    splideInstance.destroy();
+    splideInstance = null;
+  }
+};
 
-//   arrows?.forEach(arrow => {
-//     arrow.addEventListener('click', e => {
-//       const target = e.currentTarget;
+const checkViewport = () => {
+  instSecondSlider();
+  if (window.innerWidth > 980) {
+    destroySlider();
+  }
+};
 
-//       if (target.classList.contains('reviews-next')) {
-//         container.querySelector('.splide__arrow--next').click();
-//       } else {
-//         container.querySelector('.splide__arrow--prev').click();
-//       }
-//     });
-//   });
-// }
+checkViewport();
+window.addEventListener('resize', checkViewport);
